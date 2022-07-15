6d5e8d9a5ca2eb33ffb557f9efa3a5407e16a3fc
Matthew <kagzgenius@gmail.com>
2022-07-15 17:07:09 -0500

    Fixed build by inserting styles.ccs in /src dir

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import React from "react";
 import * as ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css"
 import App from "./src/components/app"
-import React from "react";
 
 const root = ReactDOM.createRoot(
     document.getElementById("app")
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+function App () {
+    return (  
+        <div>
+            <h1>
+                Make payments in your local currency!
+            </h1>
+        </div>
+    );
+}
+
+export default App ;
